94b3d32d4819cd1ef194206a75a762949b14e02e
Kelvin Tan <kelv@discourse.org>
2024-06-19 21:52:31 +0800

    DEV: add db migration to filter out invalid csp script source values

---
+++ db/migrate/20240619080454_remove_invalid_csp_script_src_site_setting_values.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class RemoveInvalidCspScriptSrcSiteSettingValues < ActiveRecord::Migration[7.0]
+  def up
+    csp_script_src_setting_value =
+      DB.query_single(
+        "SELECT value FROM site_settings WHERE name = 'content_security_policy_script_src'",
+      ).first
+
+    if csp_script_src_setting_value.present?
+      regex =
+        /
+        (?:^'unsafe-eval'$)|
+        (?:^'wasm-unsafe-eval'$)|
+        (?:^'sha(?:256|384|512)-[A-Za-z0-9+\/=]{44,88}'$)
+      /x
+      updated_value =
+        csp_script_src_setting_value
+          .split("|")
+          .select { |substring| substring.match?(regex) }
+          .uniq
+          .join("|")
+
+      DB.exec(<<~SQL, updated_value:)
+        UPDATE site_settings
+        SET value = :updated_value
+        WHERE name = 'content_security_policy_script_src'
+      SQL
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
